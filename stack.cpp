aaab13d897a4c2e9f1aacbafbc8ab7b6559bc603
i-i-ijul <haqqikamvret@gmail.com>
2025-05-22 10:27:13 +0700

    menambahkan while

--- stack.cpp
+++ stack.cpp
@@ -82,6 +82,20 @@ int main()
 
     while (choice != 5)
     {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4.Exit\n";
+        cout << "enter ur choice: ";
+        cin >> choice;
 
+        switch (choice)
+        {
+            case 1:
+            cout << "enter the value to push: ";
+            cin >> value;
+            stack.push(value); //push the entered value onto the stack
+            break;
+        }
     }
 }
